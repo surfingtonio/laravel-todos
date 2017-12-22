ab56c7a10a6b4ee65c32e056204a9d1e15d4c98d
Marc Anthony Danting <marc.anthony@danting.ca>
2017-12-22 23:34:05 +0000

    Code clean up

--- routes/web.php
+++ routes/web.php
@@ -11,34 +11,4 @@
 |
 */
 
-/*Route::get('/', function() {
-	return view('todo.todos');
-});*/
-
 Route::get('/', 'TodoController@index');
-
-/*
-Route::post('/todos/create', [
-	'uses' => 'TodoController@store',
-	'as' => 'todo.create'
-]);
-
-Route::post('/todos/delete/{id}', [
-	'uses' => 'TodoController@destroy',
-	'as' => 'todo.delete'
-]);
-
-Route::get('/todos/complete/{id}', [
-	'uses' => 'TodoController@complete',
-	'as' => 'todos.complete'
-]);
-
-Route::get('/todos/restore/{id}', [
-	'uses' => 'TodoController@restore',
-	'as' => 'todos.restore'
-]);
-
-Route::get('/todos/delete/{id}', [
-	'uses' => 'TodoController@destroy',
-	'as' => 'todos.delete'
-]);*/
